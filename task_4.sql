9d7ebecede133a15b3062202fc5fb8430f15458b
Ayman <ayman.mouhcine1995@gmail.com>
2025-07-05 01:23:41 +0100

    MySQL script

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,12 @@
 SELECT 
-    column_name, 
+    column_name,
     column_type, 
     is_nullable, 
     column_key, 
     column_default, 
     extra
 FROM 
-    information_schema.columns
+    INFORMATION_SCHEMA.COLUMNS
 WHERE 
     table_schema = DATABASE()
     AND table_name = 'books'
