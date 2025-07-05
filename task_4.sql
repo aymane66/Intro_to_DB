a9e4da352901787d31f9c61d9713f3403e5e2560
Ayman <ayman.mouhcine1995@gmail.com>
2025-07-05 01:24:27 +0100

    MySQL script

--- task_4.sql
+++ task_4.sql
@@ -1,14 +1,14 @@
-SELECT 
-    column_name,
+select 
+    column_name, 
     column_type, 
     is_nullable, 
     column_key, 
     column_default, 
     extra
-FROM 
-    INFORMATION_SCHEMA.COLUMNS
-WHERE 
-    table_schema = DATABASE()
-    AND table_name = 'books'
-ORDER BY 
+from 
+    information_schema.columns
+where 
+    table_schema = database()
+    and table_name = 'books'
+order by 
     ordinal_position;
